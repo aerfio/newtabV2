5f991570baeb8f0f177bf2e62fff944ed12fd295
Aerfio <mati6095@gmail.com>
2018-09-16 18:25:22 +0200

    further fun with mobx

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import { observer } from 'mobx-react';
 import { action } from 'mobx';
-if (process.env.NODE_ENV !== 'production') {
-	const { whyDidYouUpdate } = require('why-did-you-update');
-	whyDidYouUpdate(React);
-}
+// if (process.env.NODE_ENV !== 'production') {
+// 	const { whyDidYouUpdate } = require('why-did-you-update');
+// 	whyDidYouUpdate(React);
+// }
 class App extends Component {
 	render() {
-		const { todos, todoCount } = this.props.store;
+		const { todos, myStr } = this.props.store;
 		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
+					<h1 className="App-title">{'Welcome to React ' + myStr}</h1>
 				</header>
 				{todos.map((e, idx) => (
 					<p key={idx}>{e}</p>
 				))}
 				<button onClick={action('push', () => todos.push('xD'))}>xD</button>
-				<button onClick={() => console.log(todoCount)}>lol</button>
+				<button onClick={action('changeString', () => myStr.set('lol'))}>lol</button>
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -1,13 +1,14 @@
 import { observable, computed, decorate, autorun, reaction } from 'mobx';
 export class MainStore {
 	todos = ['as', 'ds'];
-
+	myStr = observable.box('myStr');
 	get todoCount() {
 		return this.todos.length;
 	}
 }
 decorate(MainStore, {
 	todos: observable,
+	myStr: observable,
 	todoCount: computed,
 });
 
@@ -15,10 +16,10 @@ const store = new MainStore();
 // autorun(() => {
 // 	console.log(store.todos.length);
 // });
-reaction(
-	() => store.todos.length,
-	() => {
-		store.todos.forEach(console.log);
-	},
-);
+// reaction(
+// 	() => store.todos.length,
+// 	() => {
+// 		store.todos.forEach(console.log);
+// 	},
+// );
 export default store;
