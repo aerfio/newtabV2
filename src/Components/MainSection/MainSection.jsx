486684a1e8db8a8477aad5824a0f9acf6dd6d621
Aerfio <mati6095@gmail.com>
2018-10-09 21:18:31 +0200

    change gitignore

--- src/Components/MainSection/MainSection.jsx
+++ src/Components/MainSection/MainSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './Navbar/Navbar';
 import Searchbar from './Searchbar/Searchbar';
 import MainSectionContent from './MainSectionContent/MainSectionContent';
-import './MainSection.css';
+
 import styled from 'styled-components';
 const Wrapper = styled.section`
 	grid-area: mainSection;
