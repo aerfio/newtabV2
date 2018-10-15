56955bdc9bed7669d66405ccb03bc73ee97253fb
Mateusz Puczynski <mateusz.puczynski@sap.com>
2018-10-15 17:26:07 +0200

    Shorten those two messages

--- src/Components/EvenOrOddWeek/EvenOrOddWeek.jsx
+++ src/Components/EvenOrOddWeek/EvenOrOddWeek.jsx
@@ -3,34 +3,36 @@ import React from 'react';
 import styled from 'styled-components';
 import colors from './../../colors';
 const EvenOrOddWeekContainer = styled.div`
-	grid-area: even_or_odd;
-	text-align: center;
+    grid-area: even_or_odd;
+    text-align: center;
 `;
 const Text = styled.h1`
-	color: ${colors.text_color};
-	font-family: 'Noto Serif', 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-	font-size: 2.5em;
-	padding-top: 20px;
-	padding-right: 20px;
+    color: ${colors.text_color};
+    font-family: 'Noto Serif', 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-size: 2.5em;
+    padding-top: 20px;
+    padding-right: 20px;
 `;
 const EvenOrOddWeek = () => {
-	const parity = getWeek() % 2 === 0;
+    const parity = getWeek() % 2 === 0;
 
-	return (
-		<EvenOrOddWeekContainer>
-			<Text>{`Jest ${parity ? '' : 'nie'}parzysty tydzień`}</Text>
-		</EvenOrOddWeekContainer>
-	);
+    return (
+        <EvenOrOddWeekContainer>
+            <Text>{`Tydzień ${parity ? '' : 'nie'}parzysty`}</Text>
+        </EvenOrOddWeekContainer>
+    );
 };
 export default EvenOrOddWeek;
 
 const getWeek = function() {
-	var date = new Date();
-	date.setHours(0, 0, 0, 0);
-	// Thursday in current week decides the year.
-	date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
-	// January 4 is always in week 1.
-	var week1 = new Date(date.getFullYear(), 0, 4);
-	// Adjust to Thursday in week 1 and count number of weeks from date to week1.
-	return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7);
+    var date = new Date();
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return (
+        1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + ((week1.getDay() + 6) % 7)) / 7)
+    );
 };
