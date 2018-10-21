86806166e11a8e7cd8487ce5a9f4afe034123ae9
Mateusz Puczynski <mateusz.puczynski@sap.com>
2018-10-21 17:50:39 +0200

    Hide calendar

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -7,42 +7,45 @@ import colors from './colors';
 import store from './stores/MainStore';
 
 const Wrapper = styled.section`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	background-color: ${colors.primary};
-	height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: ${colors.primary};
+    height: 100vh;
 `;
 const Button = styled.button`
-	padding: 10px;
-	color: ${colors.text_color};
-	background-color: ${colors.primary};
-	border: 1px solid ${colors.text_color};
-	outline: none;
-	font-size: 1.2em;
+    padding: 10px;
+    color: ${colors.text_color};
+    background-color: ${colors.primary};
+    border: 1px solid ${colors.text_color};
+    outline: none;
+    font-size: 1.2em;
 `;
 const IframeWrapper = styled.div`
-	margin: 20px 0px 20px 0px;
-	width: 85%;
-	height: 80%;
+    margin: 20px 0px 20px 0px;
+    width: 85%;
+    height: 80%;
 `;
 const calendarUrl = process.env.REACT_APP_CALENDAR_IFRAME;
 const Calendar = () => {
-	return (
-		<Wrapper>
-			<IframeWrapper>
-				<Iframe url={calendarUrl} position="relative" />
-			</IframeWrapper>
-			<Button
-				onClick={() => {
-					runInAction('change to main view', () => {
-						store.page = 'Main';
-					});
-				}}>
-				Powrót
-			</Button>
-		</Wrapper>
-	);
+    if (calendarUrl)
+        return (
+            <Wrapper>
+                <IframeWrapper>
+                    <Iframe url={calendarUrl} position="relative" />
+                </IframeWrapper>
+                <Button
+                    onClick={() => {
+                        runInAction('change to main view', () => {
+                            store.page = 'Main';
+                        });
+                    }}
+                >
+                    Powrót
+                </Button>
+            </Wrapper>
+        );
+    return null;
 };
 
 export default observer(Calendar);
--- src/Components/MainSection/Navbar/Navbar.jsx
+++ src/Components/MainSection/Navbar/Navbar.jsx
@@ -5,20 +5,22 @@ import { observer } from 'mobx-react';
 import styled from 'styled-components';
 
 const NavbarWrapper = styled.div`
-	display: flex;
-	justify-content: center;
+    display: flex;
+    justify-content: center;
 `;
 
 class Navbar extends Component {
-	render() {
-		return (
-			<NavbarWrapper>
-				<Navbutton text="Linki" store={store} />
-				<Navbutton text="Notatki" store={store} />
-				<Navbutton pageChange text="Kalendarz" store={store} />
-			</NavbarWrapper>
-		);
-	}
+    render() {
+        return (
+            <NavbarWrapper>
+                <Navbutton text="Linki" store={store} />
+                <Navbutton text="Notatki" store={store} />
+                {process.env.REACT_APP_CALENDAR_IFRAME && (
+                    <Navbutton pageChange text="Kalendarz" store={store} />
+                )}
+            </NavbarWrapper>
+        );
+    }
 }
 
 export default observer(Navbar);
