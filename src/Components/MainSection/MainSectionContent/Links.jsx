bba11da71c4a55da6d3853418f168dd92f7a98f5
Mateusz Puczynski <mateusz.puczynski@sap.com>
2018-10-21 18:57:18 +0200

    delete target blank as it was annoying

--- src/Components/MainSection/MainSectionContent/Links.jsx
+++ src/Components/MainSection/MainSectionContent/Links.jsx
@@ -4,52 +4,52 @@ import styled, { keyframes } from 'styled-components';
 import colors from '../../../colors';
 
 const links = {
-	Facebook: 'https://www.facebook.com/home.php',
-	Onet: 'https://www.onet.pl/',
-	'/mu/': 'https://boards.4chan.org/mu/',
-	Pogoda: 'https://www.google.pl/search?q=pogoda&cad=h',
-	Platforma: 'https://platforma.polsl.pl/rau1/course/index.php?categoryid=26',
-	'Google Drive': 'https://drive.google.com/drive/my-drive',
-	'Img Search': 'https://images.google.com/',
+    Facebook: 'https://www.facebook.com/home.php',
+    Onet: 'https://www.onet.pl/',
+    '/mu/': 'https://boards.4chan.org/mu/',
+    Pogoda: 'https://www.google.pl/search?q=pogoda&cad=h',
+    Platforma: 'https://platforma.polsl.pl/rau1/course/index.php?categoryid=26',
+    'Google Drive': 'https://drive.google.com/drive/my-drive',
+    'Img Search': 'https://images.google.com/',
 };
 
 const Header = styled.h1`
-	text-align: center;
-	transition-duration: 0.15s;
-	color: ${colors.text_color};
-	font-family: 'Noto Serif', serif;
-	letter-spacing: 3px;
-	font-weight: 700;
-	font-size: 2em;
-	letter-spacing: 2px;
-	margin-top: 10px;
-	margin-bottom: 15px;
+    text-align: center;
+    transition-duration: 0.15s;
+    color: ${colors.text_color};
+    font-family: 'Noto Serif', serif;
+    letter-spacing: 3px;
+    font-weight: 700;
+    font-size: 2em;
+    letter-spacing: 2px;
+    margin-top: 10px;
+    margin-bottom: 15px;
 `;
 const List = styled.ul`
-	transition-duration: 0.15s;
-	list-style-type: none;
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	padding-inline-start: 0;
-	margin-bottom: 20px;
-	margin-top: 20px;
+    transition-duration: 0.15s;
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding-inline-start: 0;
+    margin-bottom: 20px;
+    margin-top: 20px;
 `;
 const ListElement = styled.li`
-	text-align: center;
-	transition-duration: 0.15s;
+    text-align: center;
+    transition-duration: 0.15s;
 `;
 const Link = styled.a`
-	color: ${colors.text_color};
-	transition-duration: 0.15s;
-	font-family: 'Noto Serif', serif;
-	text-decoration: none;
-	&:visited,
-	&:hover {
-		text-decoration: none;
-	}
-	font-size: 1.2em;
-	letter-spacing: 0.15px;
+    color: ${colors.text_color};
+    transition-duration: 0.15s;
+    font-family: 'Noto Serif', serif;
+    text-decoration: none;
+    &:visited,
+    &:hover {
+        text-decoration: none;
+    }
+    font-size: 1.2em;
+    letter-spacing: 0.15px;
 `;
 const fadeIn = keyframes`from{
 opacity:0;
@@ -57,33 +57,31 @@ opacity:0;
 	opacity:1;
 }`;
 const Wrapper = styled.section`
-	width: 50%;
-	transition-duration: 0.15s;
-	animation: ${fadeIn} 0.2s;
-	border-bottom: 1px solid ${colors.text_color};
-	&:hover {
-		background-color: ${colors.text_color};
-	}
-	&:hover ${Header}, &:hover ${Link} {
-		color: ${colors.primary};
-	}
+    width: 50%;
+    transition-duration: 0.15s;
+    animation: ${fadeIn} 0.2s;
+    border-bottom: 1px solid ${colors.text_color};
+    &:hover {
+        background-color: ${colors.text_color};
+    }
+    &:hover ${Header}, &:hover ${Link} {
+        color: ${colors.primary};
+    }
 `;
 const Links = () => {
-	return (
-		<Wrapper>
-			<List>
-				{Object.keys(links).map((el, index) => {
-					return (
-						<ListElement key={index}>
-							<Link href={links[el]} target={'_blank'}>
-								{el}
-							</Link>
-						</ListElement>
-					);
-				})}
-			</List>
-		</Wrapper>
-	);
+    return (
+        <Wrapper>
+            <List>
+                {Object.keys(links).map((el, index) => {
+                    return (
+                        <ListElement key={index}>
+                            <Link href={links[el]}>{el}</Link>
+                        </ListElement>
+                    );
+                })}
+            </List>
+        </Wrapper>
+    );
 };
 
 export default Links;
