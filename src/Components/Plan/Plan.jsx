b439e7dc6e6a23e07cab0603b4606a0af2a27547
Mateusz Puczyński <mati6095@gmail.com>
2019-06-15 19:24:19 +0200

    load image only onhover

--- src/Components/Plan/Plan.jsx
+++ src/Components/Plan/Plan.jsx
@@ -2,42 +2,44 @@ import React from "react";
 import plan from "./plan.png";
 import colors from "./../../colors";
 import styled from "styled-components";
-
+import ReactHoverObserver from "react-hover-observer";
 const PlanWrapper = styled.div`
-    z-index: 1000;
-    position: fixed;
-    top: 25px;
-    right: -750px;
-    width: 750px;
-    transition-duration: 0.25s;
-    &::before {
-        content: "";
-        display: block;
-        height: 433px;
-        width: 7px;
-        background-color: ${colors.accent_color};
-        margin-top: 80px;
-        float: left;
-        margin-left: -7px;
-        border-bottom-left-radius: 3px;
-        border-top-left-radius: 3px;
-    }
-    &:hover {
-        right: -2px;
-    }
+	z-index: 1000;
+	position: fixed;
+	top: 25px;
+	right: ${props => (props.isHovered ? "-2px" : "-750px")};
+	width: 750px;
+	transition-duration: 0.25s;
+	&::before {
+		content: "";
+		display: block;
+		height: 433px;
+		width: 7px;
+		background-color: ${colors.accent_color};
+		margin-top: 80px;
+		float: left;
+		margin-left: -7px;
+		border-bottom-left-radius: 3px;
+		border-top-left-radius: 3px;
+	}
 `;
+
 const Image = styled.img`
-    margin-top: 80px;
-    max-width: 100%;
-    object-fit: cover;
-    object-position: -3px -2px;
+	margin-top: 80px;
+	max-width: 100%;
+	object-fit: cover;
+	object-position: -3px -2px;
 `;
 const Plan = () => {
-    return (
-        <PlanWrapper>
-            <Image src={plan} alt={"my_plan_for_uni"} />;
-        </PlanWrapper>
-    );
+	return (
+		<ReactHoverObserver>
+			{({ isHovering }) => (
+				<PlanWrapper isHovered={isHovering}>
+					{isHovering && <Image src={plan} alt={"my_plan_for_uni"} />}
+				</PlanWrapper>
+			)}
+		</ReactHoverObserver>
+	);
 };
 
 export default Plan;
